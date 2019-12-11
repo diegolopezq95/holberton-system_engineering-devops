8a671d4c17da883879de10abc127436f58b3fdcf
diegolopezq95 <927@holbertonschool.com>
2019-12-11 16:41:36 +0000

    install puppet

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -4,14 +4,14 @@ package { 'nginx':
   ensure => 'present',
 }
 exec { 'display':
-  path    => ['/usr/bin', '/bin'],
+  path    => ['/usr/bin', '/usr/sbin', '/bin'],
   command => 'echo "Holberton School" | sudo tee /var/www/html/index.html',
 }
 exec { 'change':
-  path    => ['/usr/bin', '/bin'],
-  command => 'sed -i "s/server_name _;/server_name _;\n\trewrite ^\/redirect_me https:\/\/www.google.com\/ permanent;/" /etc/nginx/sites-available/default',
+  path    => ['/usr/bin', '/usr/sbin', '/bin'],
+  command => 'sed -i "s/server_name _;/server_name _;\n\trewrite ^\/redirect_me https:\/\/www.google.com\/ permanent;/" /etc/nginx/sites-enabled/default',
 }
 exec { 'start_nginx':
-  path    => ['/usr/bin', '/bin'],
-  command => 'sudo service nginx start',
+  path    => ['/usr/bin', '/usr/sbin', '/bin'],
+  command => 'sudo service nginx restart',
 }
