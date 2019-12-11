2be3111a1fa5ce44f5f0b6ed68a6e31a302aa6b9
diegolopezq95 <927@holbertonschool.com>
2019-12-11 18:25:32 +0000

    Install Nginx web server (w/ Puppet)

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,10 +1,10 @@
 # Install Nginx web server (w/ Puppet)
 
 exec {'nginx_install':
-  command  => 'sudo apt-get update && sudo apt-get -y install nginx && echo "Holberton School" | sudo tee /var/www/html/index.html',
+  command  => 'sudo apt-get update ; sudo apt-get -y install nginx ; echo "Holberton School" | sudo tee /var/www/html/index.html',
   provider => shell,
 }
 exec {'nginx_full':
-  command  => 'new_string="server_name _;\n\trewrite ^\/redirect_me https:\/\/www.youtube.com\/watch?v=QH2-TGUlwu4 permanent;" && sudo sed -i "s/server_name _;/$new_string/" /etc/nginx/sites-available/default && sudo service nginx restart',
+  command  => 'new_string="server_name _;\n\trewrite ^\/redirect_me https:\/\/www.youtube.com\/watch?v=QH2-TGUlwu4 permanent;" ; sudo sed -i "s/server_name _;/$new_string/" /etc/nginx/sites-available/default ; sudo service nginx restart',
   provider => shell,
 }
