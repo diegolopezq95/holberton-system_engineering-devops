321a0015c47c5ff2efb70556e58b88b6dac7487f
diegolopezq95 <927@holbertonschool.com>
2020-02-03 19:07:04 +0000

    export data in the JSON format (see todo_all_employees.json)

---
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+""" This module export data in the JSON format.
+"""
+
+import requests
+import sys
+import json
+from collections import OrderedDict
+
+
+if __name__ == "__main__":
+    url = "https://jsonplaceholder.typicode.com"
+    employee_url = ("{}/users".format(url))
+    r_employee = requests.get(employee_url)
+    employee_json = r_employee.json()
+    json_name = "todo_all_employees.json"
+    with open(json_name, 'w') as json_file:
+        dict_of_dicts = OrderedDict()
+        for employee in employee_json:
+            employee_id = employee.get("id")
+            employee_usrname = employee.get("username")
+            todo_list_url = ("{}/todos?userId={}".format(url, employee_id))
+            r_todo_list = requests.get(todo_list_url)
+            todo_list_json = r_todo_list.json()
+            todo_list = []
+            for task in todo_list_json:
+                task_completed = task.get("completed")
+                task_title = task.get("title")
+                todo_dict = {"username": employee_usrname,
+                             "task": task_title,
+                             "completed": task_completed}
+                todo_list.append(OrderedDict(todo_dict))
+            dict_of_dicts[str(employee_id)] = todo_list
+        json_file.write(json.dumps(dict_of_dicts))
