186fcdaed2a45aa87d1f8ea39eadb84e44b39154
diegolopezq95 <927@holbertonschool.com>
2020-02-18 20:03:46 +0000

    change phpp for php in wp-settings.php

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,5 @@
+exec { 'fix-wp-error-log':
+  command  => "sed -i 's/phpp/php/' /var/www/html/wp-settings.php",
+  path     => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin'
+  provider => shell,
+}
