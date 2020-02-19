6bc7b74e37552e85b903a96bca596267b61fe555
diegolopezq95 <927@holbertonschool.com>
2020-02-19 14:22:03 +0000

    [Fix]: puppet-lint and comment

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,5 @@
+# fixing type in wp-settings.php file
 exec { 'fix-wp-error-log':
-  command  => "sed -i 's/phpp/php/' /var/www/html/wp-settings.php",
-  path     => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin'
-  provider => shell,
+  command => "sed -i 's/phpp/php/' /var/www/html/wp-settings.php",
+  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin'
 }
